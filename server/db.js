1d2c1e707c99e29be4af950eb010f0dda891a0e5
nejivn <phonggalaxy8@gmail.com>
2025-03-22 22:13:52 +0700

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'your_username',
-    host: 'localhost',
-    database: 'your_database',
-    password: 'your_password',
-    port: 5432,
+    host: "poignantly-droll-chamois.data-1.use1.tembo.io",
+    user: "postgres",
+    password: "0YOdJV9QB2iit6Cn",
+    database: "public",
+    port: 5432
 });
 
 module.exports = pool;
